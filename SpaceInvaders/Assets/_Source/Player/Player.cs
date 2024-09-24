c0b42242e9028adaafe4c75175c0ad056ffe8d6d
FallenAng3I <s4mararunner@mail.ru>
2024-09-24 19:03:30 +0300

    feat: Movement to right and left is done
    And TMPro imported

---
+++ SpaceInvaders/Assets/_Source/Player/Player.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public float speed;
+}
---
+++ SpaceInvaders/Assets/_Source/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Player player;
+    
+    public float boundaryX = 3f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        Vector3 movement = new Vector3(horizontalInput, 0, 0) * (player.speed * Time.deltaTime);
+        transform.position += movement;
+        
+        var newPosition = transform.position.x;
+        newPosition = Mathf.Clamp(newPosition, -boundaryX, boundaryX);
+        transform.position = new Vector3(newPosition, transform.position.y, transform.position.z);
+    }
+}
